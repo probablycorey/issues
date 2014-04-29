8de32617471d99f052c5113ca844956d55224f69
probablycorey <cj@github.com>
2014-04-28 17:49:48 -0700

    DOGFOOD

--- app/js/services.js
+++ app/js/services.js
@@ -5,7 +5,7 @@ var notifyService = angular.module('myApp.services', []);
 notifyService.factory('github', ['$http', function($http) {
   return {
     issues: function() {
-      return $http({method: 'GET', url: 'https://api.github.com/repos/atom/find-and-replace/issues'}).
+      return $http({method: 'GET', url: 'https://api.github.com/repos/probablycorey/issues/issues'}).
         catch(function(data, status, headers, config) {
           console.log(status);
           throw new Error(data);
