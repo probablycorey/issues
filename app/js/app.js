242881422ebe74c966e5877fa9217fb0356f886f
probablycorey <cj@github.com>
2014-04-29 06:58:39 -0700

    Movement and animations

--- app/js/app.js
+++ app/js/app.js
@@ -4,14 +4,13 @@
 // Declare app level module which depends on filters, and services
 angular.module('myApp', [
   'ngRoute',
+  'ngAnimate',
   'myApp.services',
   'myApp.controllers',
   'cfp.hotkeys'
 ]).
 config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
   $locationProvider.html5Mode(true);
-
   $routeProvider.when('/issues', {templateUrl: 'app/partials/issues.html', controller: 'IssuesCtrl'});
   $routeProvider.otherwise({redirectTo: '/issues'});
-
 }]);
