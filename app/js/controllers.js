9b256143e4f1b5cb5b12fa8eaa726ba588ab3bc3
probablycorey <cj@github.com>
2014-04-29 21:02:04 -0700

    Move logic back to controller

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,7 +3,65 @@
 /* Controllers */
 
 angular.module('issuesApp.controllers', [])
-  .controller('IssuesCtrl', ['$scope', 'IssueService', function($scope, IssueService) {
+  .controller('IssuesCtrl', function($scope, $window, IssueService, ScrollToElementService, hotkeys) {
+    $scope.activeIssueId = null;
     $scope.currentIssues = IssueService("current");
     $scope.nextIssues = IssueService("next");
-  }]);
+    $scope.currentIssues.$on("loaded", function() {
+      $scope.activeIssueId = $scope.currentIssues.$getIndex()[0];
+    });
+
+    var activeIssues = $scope.currentIssues;
+
+    var getActiveIssue = function() {
+      return activeIssues[$scope.activeIssueId];
+    };
+
+    var selectIssueByDelta = function(delta) {
+      var ids = activeIssues.$getIndex();
+      var index = ids.indexOf($scope.activeIssueId) + delta;
+
+      if (index >= ids.length) $scope.activeIssueId = activeIssues.$getIndex()[0];
+      else if (index < 0) $scope.activeIssueId = ids[ids.length - 1];
+      else $scope.activeIssueId = ids[index];
+
+      ScrollToElementService("issue" + getActiveIssue().$id);
+    };
+
+    var moveActiveIssueByDelta = function(delta) {
+      if ($scope.activeIssueIndex + delta < 0 || $scope.activeIssueIndex + delta >= activeIssues.$getIndex().length) return;
+
+      var issue = getActiveIssue();
+      var otherIssueId = activeIssues.$getIndex()[$scope.activeIssueIndex + delta];
+      var otherIssue = activeIssues[otherIssueId];
+      var tmp = issue.$priority;
+      issue.$priority = otherIssue.$priority;
+      otherIssue.$priority = tmp;
+
+      IssueService.$save();
+    };
+
+    hotkeys.add({
+      combo: 'j',
+      description: 'Select down',
+      callback: function() {selectIssueByDelta(1); return true;}
+    });
+
+    hotkeys.add({
+      combo: 'ctrl+j',
+      description: 'Move down',
+      callback: function() {moveActiveIssueByDelta(1);}
+    });
+
+    hotkeys.add({
+      combo: 'k',
+      description: 'Select up',
+      callback: function() {selectIssueByDelta(-1);}
+    });
+
+    hotkeys.add({
+      combo: 'ctrl+k',
+      description: 'Move up',
+      callback: function() {moveActiveIssueByDelta(-1);}
+    });
+  });
--- app/js/directives.js
+++ app/js/directives.js
@@ -6,76 +6,9 @@ angular.module('issuesApp.directives', []).
     return {
       restrict: 'E',
       scope: {
-        issues: '=content'
+        issues: '=issues',
+        activeIssueId: '=activeIssueId'
       },
-      templateUrl: 'app/partials/issues.html',
-      controller: function($scope, $window, IssueService, ScrollToElementService, hotkeys) {
-        $scope.activeIssueId = null;
-
-        var getActiveIssue = function() {
-          return $scope.issues[$scope.activeIssueId];
-        };
-
-        var selectIssueByDelta = function(delta) {
-          var ids = $scope.issues.$getIndex();
-          var index = ids.indexOf($scope.activeIssueId) + delta;
-
-          if (index >= ids.length) $scope.activeIssueId = $scope.issues.$getIndex()[0];
-          else if (index < 0) $scope.activeIssueId = ids[ids.length - 1];
-          else $scope.activeIssueId = ids[index];
-
-          ScrollToElementService("issue" + getActiveIssue().$id);
-        };
-
-        var moveActiveIssueByDelta = function(delta) {
-          if ($scope.activeIssueIndex + delta < 0 || $scope.activeIssueIndex + delta >= $scope.issues.$getIndex().length) return;
-
-          var issue = getActiveIssue();
-          var otherIssueId = $scope.issues.$getIndex()[$scope.activeIssueIndex + delta];
-          var otherIssue = $scope.issues[otherIssueId];
-          var tmp = issue.$priority;
-          issue.$priority = otherIssue.$priority;
-          otherIssue.$priority = tmp;
-
-          IssueService.$save();
-        };
-
-        hotkeys.add({
-          combo: 'j',
-          description: 'Select down',
-          callback: function() {selectIssueByDelta(1);}
-        });
-
-        hotkeys.add({
-          combo: 'ctrl+j',
-          description: 'Move down',
-          callback: function() {moveActiveIssueByDelta(1);}
-        });
-
-        hotkeys.add({
-          combo: 'k',
-          description: 'Select up',
-          callback: function() {selectIssueByDelta(-1);}
-        });
-
-        hotkeys.add({
-          combo: 'ctrl+k',
-          description: 'Move up',
-          callback: function() {moveActiveIssueByDelta(-1);}
-        });
-
-        var addIssue = function() {
-          var title = $window.prompt("Create new issue", "cool");
-          if (title) {
-            $scope.issues.$add({title: title});
-          }
-        };
-
-        hotkeys.add({
-          combo: 'a',
-          description: 'Add issue',
-          callback: addIssue
-        });
-      }
+      templateUrl: 'app/partials/issues.html'
     };
   });
